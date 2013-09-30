c20e1d6c07af8c867f37162a59e8e0ff166faf21
Ian Denhardt <ian@zenhack.net>
2013-09-30 00:09:58 -0400

    Added an overview comment to errcode.h
    Explains what the file is for, and how other code should use it.

--- src/errcode.h
+++ src/errcode.h
@@ -4,6 +4,21 @@
 extern "C" {
 #endif
 
+/*
+	Various error codes used throughout skilisp.
+
+	Note that these *must* have negative values, as the typical way to
+	check for an error is:
+
+		if(foo() < 0) {
+			handle_error();
+		}
+
+	Each function that uses these should specify which codes are actually
+	possible. The meaning of each code is specified just above its
+	definition (in this file).
+*/
+
 /* The memory allocator returned an error. */
 #define SKIL_ENOMEM -1
 /* The input stream contained an invalid token. */
