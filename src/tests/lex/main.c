46c84f0cbcd8d8297c13029332331223fc1e3415
Ian Denhardt <ian@zenhack.net>
2013-10-03 19:46:59 -0400

    Some initial work on a test suite.
    TODO:
     - integrate this into the buid system
     - flesh out how this is going to work in more detail.

---
+++ src/tests/lex/main.c
@@ -0,0 +1,33 @@
+#include "lex.h"
+#include <stdlib.h>
+
+int main(void) {
+	char *img;
+	int token_type;
+
+	token_type = skil_token(stdin, &img);
+	while(token_type >= 0) {
+		switch(token_type) {
+		case '(':
+		case ')':
+			printf("%c\n", token_type);
+			break;
+		case SKIL_ATOM:
+			printf("ATOM: %s\n", img);
+			free(img);
+			break;
+		case SKIL_EOF:
+			return 0;
+			break;
+		default:
+			printf("Unknown token type %d with value %s!\n",
+				token_type, (img)? img :"NULL");
+			return 1;
+			break;
+		}
+		token_type = skil_token(stdin, &img);
+	}
+	/* this is dead code, but it quiets a warning about a missing return
+	 * value: */
+	return 0;
+}
